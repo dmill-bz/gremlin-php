2a4bf669eefe2e2d22cee42cdb00efc41b231f7f
Dylan Millikin <dylan.millikin@brightzone.fr>
2015-06-25 13:27:22 +0200

    Fixed composer call in travis

--- build/install-server.sh
+++ build/install-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --branch=3.0.0-incubating-rc1 --depth=1 https://github.com/apache/incubator-tinkerpop.git $HOME
+git clone --branch=3.0.0-incubating-rc1 --depth=1 https://github.com/apache/incubator-tinkerpop.git $HOME/incubator-tinkerpop
 cd $HOME/incubator-tinkerpop/
 mvn clean install -Dmaven.test.skip=true
 unzip $HOME/incubator-tinkerpop/gremlin-server/target/apache-gremlin-server-3.0.0-incubating-distribution.zip $HOME
